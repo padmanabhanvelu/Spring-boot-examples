250babb4f2e7277b7dc803073907434074ba2376
padmanabhanvelu <31431705+padmanabhanvelu@users.noreply.github.com>
2018-05-16 08:02:51 +0530

    Authentication with PostgresSQL
    Authentication with PostgresSQL

---
+++ SpringSecurityJDBCAuthenticationPostgreSQL/src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/SpringSecurityJdbcAuthenticationPostgreSqlApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcAuthenticationPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcAuthenticationPostgreSqlApplication.class, args);
+	}
+}
---
+++ SpringSecurityJDBCAuthenticationPostgreSQL/src/main/java/com/javasampleapproach/springsecurity/jdbcauthentication/controller/WebController.java
@@ -0,0 +1,33 @@
+package com.javasampleapproach.springsecurity.jdbcauthentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class WebController {
+   
+    @RequestMapping(value="/")
+    public String home(){
+        return "home";
+    }
+   
+    @RequestMapping(value="/user")
+    public String user(){
+        return "user";
+    }
+  
+    @RequestMapping(value="/admin")
+    public String admin(){
+        return "admin";
+    }
+   
+    @RequestMapping(value="/login")
+    public String login(){
+        return "login";
+    }
+   
+    @RequestMapping(value="/403")
+    public String Error403(){
+        return "403";
+    }
+}
